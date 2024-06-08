e245052c48a0b8f298b93c35a190f4fac413be47
Halil İbrahim <haykut19@posta.pau.edu.tr>
2024-06-08 19:04:26 +0300

    change

--- src/main/java/com/haiykut/ardecorifycore/configurations/JwtProperties.java
+++ src/main/java/com/haiykut/ardecorifycore/configurations/JwtProperties.java
@@ -1,15 +1,15 @@
 package com.haiykut.ardecorifycore.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
+    @Value("${jwt.secret.key}")
     private String secretKey;
+    @Value("${jwt.expiration}")
     private Long expiration;
-
-
     public String getSecretKey() {
         return secretKey;
     }
